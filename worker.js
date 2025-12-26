2a436956a503e1b8d2679899a09a9b3434371d28
foreyeth-wq <foreyeth@gmail.com>
2025-12-26 17:39:17 +0630

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,37 @@
+export default {
+  async fetch(request, env) {
+    if (request.method === "OPTIONS") {
+      return new Response("", {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type"
+        }
+      });
+    }
+
+    if (request.method !== "POST") {
+      return new Response(
+        JSON.stringify({ status: "error", error: "POST only" }),
+        { status: 405, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    const body = await request.json();
+
+    return new Response(
+      JSON.stringify({
+        status: "ok",
+        video_url:
+          "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
+        received: body
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      }
+    );
+  }
+};
